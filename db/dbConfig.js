fa3aef006a7fd0a4136a2108cc9970dd30ec0697
Tyrell <tyrellkamara-oliver@pursuit.org>
2025-02-27 11:50:08 -0500

    chore(db): add database connection configuration

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,4 +1,11 @@
 const pgp = require("pg-promise");
-const cn = {};
+require("dotenv").config();
+const cn = {
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  database: process.env.PG_DATABASE,
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+};
 const db = pgp(cn);
 module.exports = db;
