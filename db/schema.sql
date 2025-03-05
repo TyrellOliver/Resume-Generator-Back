6a5aa8eef76013b767c6360deaf7265f35b8ea1f
Tyrell <tyrellkamara-oliver@pursuit.org>
2025-03-05 15:25:08 -0500

    feat(db): create users table
    Defined tyrellkamara-oliver table in the database schema with relevant columns

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS resume_generator_app;
+
+\c resume_generator_app;
+
+CREATE TABLE users (
+    users_id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password_hash VARCHAR(100) NOT NULL UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+);
