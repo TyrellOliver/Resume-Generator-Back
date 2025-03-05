20532f8e6957e781e415fe745291ab8a6f9561f6
Tyrell <tyrellkamara-oliver@pursuit.org>
2025-03-05 15:57:49 -0500

    feat(db): create seed data for users table

---
+++ db/seed.sql
@@ -0,0 +1,8 @@
+\c resume_generator_app;
+
+INSERT INTO users (username, email, password_hash) VALUES
+('Ty Freeman', 'example@email.com', 'password_hash1'),
+('Jane Doe', 'jane.doe@example.com', 'password_hash2'),
+('John Smith', 'john.smith@example.com', 'password_hash3'),
+('Alice Johnson', 'alice.johnson@example.com', 'password_hash4'),
+('Bob Williams', 'bob.williams@example.com', 'password_hash5');
