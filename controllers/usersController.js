23db32640afc4cbb6d61fe12e25e9b8ff0529cab
Tyrell <tyrellkamara-oliver@pursuit.org>
2025-03-06 12:45:32 -0500

    chore(scripts): add db:init and db:seed scripts to package.json

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const users = express.Router();
 
-users.get("/", (req, res) => {
+const { getAllUsers } = require("../queries/users");
+
+users.get("/", async (req, res) => {
   try {
-    res.status(200).json({ Success: "All Users" });
+    const allUsers = await getAllUsers();
+    // console.log(allUsers)
+    // res.status(200).json(allUsers);
+    res.status(200).json({success:"All Users Route"});
   } catch (error) {
+    console.log(error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
